32372c12ee7b97ff9bea30b87e49b436e9a1d971
Marcelo Almiron <marcegabal@gmail.com>
2024-07-10 10:21:36 -0300

    Commit TwitterGo

---
+++ bd/conexion.go
@@ -0,0 +1,44 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/marcegabal/twitterGo/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCn *mongo.Client
+var DatabaseName string
+
+func ConectarBd(ctx context.Context) error {
+	user := ctx.Value(models.Key("user")).(string)
+	password := ctx.Value(models.Key("password")).(string)
+	host := ctx.Value(models.Key("host")).(string)
+	connString := fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority", user, password, host)
+
+	var clientOptions = options.Client().ApplyURI(connString)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Conexion exitosa")
+	MongoCn = client
+	DatabaseName = ctx.Value(models.Key("database")).(string)
+	return nil
+}
+
+func BaseConectada() bool {
+	err := MongoCn.Ping(context.TODO(), nil)
+	return err == nil
+}
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/marcegabal/twitterGo/awsgo"
+	"github.com/marcegabal/twitterGo/bd"
+	"github.com/marcegabal/twitterGo/handlers"
+	"github.com/marcegabal/twitterGo/models"
+	"github.com/marcegabal/twitterGo/secretmanager"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var res *events.APIGatewayProxyResponse
+
+	awsgo.InicializoAws()
+
+	if !ValidoParametros() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en variables de entorno",
+			Headers: map[string]string{
+				"Content-type": "application/json",
+			},
+		}
+
+		return res, nil
+
+	}
+
+	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en lectura de secret " + err.Error(),
+			Headers: map[string]string{
+				"Content-type": "application/json",
+			},
+		}
+
+		return res, nil
+	}
+
+	path := strings.Replace(request.PathParameters["twitterGo"], os.Getenv("UrlPrefix"), "", -1)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
+
+	//Chequeo conexion a bd
+	err = bd.ConectarBd(awsgo.Ctx)
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Error en conexion a bd " + err.Error(),
+			Headers: map[string]string{
+				"Content-type": "application/json",
+			},
+		}
+
+		return res, nil
+	}
+
+	//parte final handlers
+	respApi := handlers.Manejadores(awsgo.Ctx, request)
+	if respApi.CustomResp == nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: respApi.Status,
+			Body:       respApi.Message,
+			Headers: map[string]string{
+				"Content-type": "application/json",
+			},
+		}
+
+		return res, nil
+	} else {
+		return respApi.CustomResp, nil
+	}
+}
+
+func ValidoParametros() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("BucketName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	return true
+}
