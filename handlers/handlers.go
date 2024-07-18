0a1014fbe6b26d92bdeedac19e22ad01f4705e39
Marcelo Almiron <marcegabal@gmail.com>
2024-07-18 14:54:32 -0300

    Endpoint Usuario ver perfil

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -32,6 +32,9 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 
 		case "login":
 			return routers.Login(ctx)
+
+		case "tweet":
+			return routers.GraboTweet(ctx, claim)
 		}
 
 		//
---
+++ routers/graboTweet.go
@@ -0,0 +1,43 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/marcegabal/twitterGo/bd"
+	"github.com/marcegabal/twitterGo/models"
+)
+
+func GraboTweet(ctx context.Context, claim models.Claim) models.RespApi {
+	var mensaje models.Tweet
+	var r models.RespApi
+	r.Status = 400
+
+	IDUsuario := claim.ID.Hex()
+	body := ctx.Value(models.Key("string")).(string)
+	err := json.Unmarshal([]byte(body), &mensaje)
+	if err != nil {
+		r.Message = "Error con el mensaje " + err.Error()
+		return r
+	}
+
+	registro := models.GraboTweet{
+		UserID:  IDUsuario,
+		Mensaje: mensaje.Mensaje,
+		Fecha:   time.Now(),
+	}
+	_, status, err := bd.InsertoTweet(registro)
+	if err != nil {
+		r.Message = "Error al insertar mensaje " + err.Error()
+		return r
+	}
+	if !status {
+		r.Message = "No se logro insertar mensaje " + err.Error()
+		return r
+	}
+
+	r.Status = 200
+	r.Message = "Tweet creado"
+	return r
+}
