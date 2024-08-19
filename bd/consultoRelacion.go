166ed921b1a173c6764651a0b3649f5a79dcb4b1
Marcelo Almiron <marcegabal@gmail.com>
2024-08-19 15:32:26 -0300

    Endpoint Usuario ver perfil

---
+++ bd/consultoRelacion.go
@@ -0,0 +1,27 @@
+package bd
+
+import (
+	"context"
+
+	"github.com/marcegabal/twitterGo/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func ConsultaRelacion(t models.Relacion) bool {
+	ctx := context.TODO()
+	db := MongoCn.Database(DatabaseName)
+	col := db.Collection("relacion")
+
+	condicion := bson.M{
+		"usuarioid":         t.UsuarioID,
+		"usuariorelacionid": t.UsuarioRelacionID,
+	}
+
+	var resultado models.Relacion
+	err := col.FindOne(ctx, condicion).Decode(&resultado)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
